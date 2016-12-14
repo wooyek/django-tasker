5d74c25a5edb1b21e1d496a755112f8abb2b88a1
Janusz Skonieczny <git@wooyek.com>
2016-12-14 17:49:05 +0100

    Bump version: 0.9.1 → 0.9.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = parse_requirements(
 with io.open("README.rst", encoding="UTF-8") as readme:
     long_description = readme.read()
 
-version = "0.9.1"
+version = "0.9.2"
 
 setup_kwargs = {
     'name': "django-tasker",
