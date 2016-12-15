9bdaa4df91dda91d532fc73c6df5758fd178b7c0
Janusz Skonieczny <git@wooyek.com>
2016-12-15 17:49:36 +0100

    sync branches

--- tasks.py
+++ tasks.py
@@ -68,11 +68,11 @@ def sync(ctx):
     ctx.run("git checkout develop")
     ctx.run("git merge master --verbose")
 
+
 @task(sync, bump, upload_pypi)
 def release(ctx):
-    ctx.run("git checkout master")
-    ctx.run("git merge develop --verbose")
     ctx.run("git checkout develop")
+    ctx.run("git merge master --verbose")
 
     ctx.run("git push origin develop --verbose")
     ctx.run("git push origin master --verbose")
