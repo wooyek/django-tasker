263b36d5bb1acb16e2f0a11a5255f17c985d013d
Janusz Skonieczny <git.js@bravelabs.pl>
2017-01-20 11:28:49 +0100

    Bump version: 0.2.39 → 0.2.40

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = parse_requirements(
 with io.open("README.rst", encoding="UTF-8") as readme:
     long_description = readme.read()
 
-version = "0.2.39"
+version = "0.2.40"
 
 setup_kwargs = {
     'name': "django-tasker",
